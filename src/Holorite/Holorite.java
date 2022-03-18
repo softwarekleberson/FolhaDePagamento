41c00e989113fa8b775079e7f5e1184b3d45f612
softwarekleberson <santossilvakleberson@gmail.com>
2022-03-18 17:53:19 -0300

    adicionando list em todas as classes

--- src/Holorite/Holorite.java
+++ src/Holorite/Holorite.java
@@ -1,41 +1,99 @@
 package Holorite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Trabalho.Cargo;
 import Trabalho.Empreitada;
 import Trabalho.Horista;
 
 public class Holorite {	
 	
-	private Horista horista;
-	private Cargo cargo;
-	private Empreitada empreitada;
+	private List<Horista> horista = new ArrayList<>();
+	private List<Cargo> cargo = new ArrayList<>();
+	private List <Empreitada> empreitada = new ArrayList<>();
 
-	public Holorite(Horista horista) {	
-		this.horista = horista;
+	public void informacoes() {
+		
 	}
- 
-	public Horista getHorista() {
+
+	public Holorite(List<Horista> horista, List<Cargo> cargo, List<Empreitada> empreitada) {
+		super();
+		this.horista = horista;
+		this.cargo = cargo;
+		this.empreitada = empreitada;
+	}
+
+	public List<Horista> getHorista() {
 		return horista;
 	}
 
-	public void setHorista(Horista horista) {
+	public void setHorista(List<Horista> horista) {
 		this.horista = horista;
 	}
 
-	public Cargo getCargo() {
+	public List<Cargo> getCargo() {
 		return cargo;
 	}
 
-	public void setCargo(Cargo cargo) {
+	public void setCargo(List<Cargo> cargo) {
 		this.cargo = cargo;
 	}
 
-	public Empreitada getEmpreitada() {
+	public List<Empreitada> getEmpreitada() {
 		return empreitada;
 	}
 
-	public void setEmpreitada(Empreitada empreitada) {
+	public void setEmpreitada(List<Empreitada> empreitada) {
 		this.empreitada = empreitada;
+	}
+	
+	public void addHorista(Horista obj) {
+		horista.add(obj);
+	}
+	
+	public void removeHorista(Horista obj){
+		horista.remove(obj);
+	}
+	
+	public int countHorista(){
+		return horista.size();
+	}
+	
+	public Horista getHorista(int i){
+		return horista.get(i);
+	}
+	
+	public void addCargo(Cargo obj) {
+		cargo.add(obj);
+	}
+	
+	public void removeCargo(Cargo obj){
+		cargo.remove(obj);
+	}
+	
+	public int countCargo(){
+		return cargo.size();
+	}
+	
+	public Cargo getCargo(int i){
+		return cargo.get(i);
+	}
+
+	public void addEmpreitada(Empreitada obj) {
+		empreitada.add(obj);
+	}
+	
+	public void removeEmpreitada(Empreitada obj){
+		empreitada.remove(obj);
+	}
+	
+	public int countEmpreitada(){
+		return empreitada.size();
+	}
+	
+	public Empreitada getEmpreitada(int i){
+		return empreitada.get(i);
 	}
 	
 	
